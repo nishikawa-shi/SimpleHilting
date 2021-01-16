17ebcaff0f7abe74f741675736b947ff62789555
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2021-01-16 10:13:33 +0900

    create project as fragment+viewModel

---
+++ app/src/main/java/com/ntetz/android/simplehilting/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ntetz.android.simplehilting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ntetz.android.simplehilting.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/ntetz/android/simplehilting/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.ntetz.android.simplehilting.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
